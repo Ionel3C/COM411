4d8c9f9b4d5c3b577d54f3ef3b98a9f6aa39f185
ionel chiaga <1chiai34@solent.ac.uk>
2022-01-26 15:09:20 +0000

    ascii2

---
+++ basics/output/ascii-art.py
@@ -0,0 +1,11 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+
+print("___________")
+print("| _     _  |")
+print("|    £     |")
+print("|    OO    |")
+print("___________")
---
+++ basics/output/escape_characters.py
@@ -0,0 +1,8 @@
+# Escape characters
+print("\n new line")
+print("\t new tab space")
+print("\\ back slash")
+print("\" double quote")
+print("\' single quote")
+"\n"
+print('\t', '\"'"I am Beep!\"")
