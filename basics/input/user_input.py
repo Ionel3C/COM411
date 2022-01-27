896e6d01f684e58bf75d7cebb023e5dfcf02df8b
ionel chiaga <1chiai34@solent.ac.uk>
2022-01-27 05:58:02 +0000

    user_input

---
+++ basics/input/user_input.py
@@ -0,0 +1,7 @@
+print('What is your name human?')
+name = input()
+print('It is nice to meet you human', name)
+
+# alternate way to write this code
+# name = input("What is your name human?")
+# print("Nice to meet you human", name, ".")
